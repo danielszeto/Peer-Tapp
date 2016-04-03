22b7debaccfad7b110a874218c6144dea7a47005
danielszeto <d.szeto123@gmail.com>
2016-04-02 17:21:44 -0700

    adds controller and API

---
+++ public/javascripts/app.js
@@ -0,0 +1,111 @@
+var app = angular.module('peertapp', ['ui.router', 'ngResource']);
+
+app.config(config);
+
+config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+function config($stateProvider, $urlRouterProvider, $locationProvider) {
+    console.log('config');
+    //this allows us to use routes without hash params!
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+    // for any unmatched URL redirect to /
+    $urlRouterProvider.otherwise("/");
+
+     $stateProvider
+      .state('home', {
+        url: "/",
+        controller: 'HomeController',
+        controllerAs: 'home',
+        templateUrl: "templates/home.html"
+      })
+
+      // .state('about', {
+      //   url: "/about",
+      //   controller: 'HomeController',
+      //   controllerAs: 'home',
+      //   templateUrl: "templates/about.html"
+      // 	})
+
+      //  .state('show', {
+      //   url: "/shoes/:id",
+      //   controller: 'HomeController',
+      //   controllerAs: 'home',
+      //   templateUrl: "templates/shoeshow.html"
+      // 	})
+
+      // .state('contact', {
+      //   url: "/contact",
+      //   controller: 'HomeController',
+      //   controllerAs: 'home',
+      //   templateUrl: "templates/contact.html"
+      // 	});
+  }
+
+
+
+app.controller('HomeController', HomeController);
+
+	function HomeController(Beer, Comment, $scope) {
+		this.newBeer = {};
+    this.beers = Beer.query();
+    this.createBeer = createBeer;
+    this.updateBeer = updateBeer;
+    this.deleteBeer = deleteBeer;
+    this.incrementUpvotes = incrementUpvotes;
+    this.newBeer ={};
+    this.comments = Comment.query();
+    this.createComment = createComment;
+
+	function updateBeer(beer) {
+		console.log('updating3');
+	    Beer.update({id: beer._id}, beer);
+	    this.displayEditForm = false;
+  	}
+
+  function incrementUpvotes(beer){
+      console.log('incrementing');
+      beer.upvotes += 1;
+      Beer.update({id: beer._id}, beer);
+    }
+
+	 function createBeer(){
+	 	console.log('incrementing2');
+	      Beer.save(this.newBeer);
+	      this.beers.push(this.newBeer);
+	      this.newBeer = {};
+	      console.log('saved');
+	 }
+
+  	function deleteBeer(beer) {
+	  	console.log("deleting", beer._id);
+	    Beer.remove({id:beer._id});
+	    var beersIndex = this.beers.indexOf(beer);
+	    this.beers.splice(beersIndex, 1);
+  	}
+
+    function createComment(beer) {
+      this.newComment.beerId = beer._id;
+      Comment.save(this.newComment);
+      this.comments.push(this.newComment);
+      this.newComment = {};
+      console.log('saved comment');
+    }
+}	
+
+app.service('Beer', function($resource) {
+  return $resource('http://localhost:3000/api/beers/:id', { id: '@_id' }, {
+    update: {
+      method: 'PUT' // this method issues a PUT request
+    }
+  });
+});
+
+app.service('Comment', function($resource) {
+  return $resource('http://localhost:3000/api/comments/:id', { id: '@_id' }, {
+    update: {
+      method: 'PUT' // this method issues a PUT request
+    }
+  });
+});
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    Beer = require('./models/beer'),
+    Comment = require('./models/Comments');
 
 // configure bodyParser (for receiving form data)
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,6 +19,138 @@ app.set('view engine', 'hbs');
 
 // connect to mongodb
 mongoose.connect('mongodb://localhost/peertapp');
+
+app.get('/api/beers', function (req, res) {
+    Beer.find(function (err, allBeers) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json(allBeers);
+    }
+  });
+});
+
+app.post('/api/beers', function (req, res) {
+    var newBeer = new Beer(req.body);
+  newBeer.save(function (err, savedBeer) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json(savedBeer);
+    }
+  });
+});
+
+app.get('/api/beers/:id', function (req, res) {
+// get beer id from url params (`req.params`)
+  var beerId = req.params.id;
+
+
+  // find beer in db by id
+  Beer.findOne({ _id: beerId }, function (err, foundBeer) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json(foundBeer);
+    }
+  });
+
+
+});
+
+//update Beer
+app.put('/api/beers/:id', function (req, res) {
+    var id = req.params.id;
+    console.log('hit update route');
+    Beer.findById({_id: id}, function (err, foundBeer){
+        if (err) console.log(err);
+        foundBeer.name = req.body.name;
+        foundBeer.type = req.body.type;
+        foundBeer.style = req.body.style;
+        foundBeer.upvotes = req.body.upvotes;
+        foundBeer.save(function (err, saved){
+            if (err) { console.log(err);}
+            res.json(saved);
+        });
+    });
+});
+
+app.delete('/api/beers/:id', function (req, res) {
+	var id = req.params.id;
+	Beer.remove({_id:id}, function (err) {
+		if (err)
+		console.log(err);
+  });
+});
+
+//comments
+app.get('/api/comments', function (req, res) {
+    Comment.find(function (err, allComments) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json(allComments);
+    }
+  });
+});
+
+app.post('/api/comments', function (req, res) {
+    var newComment = new Comment(req.body);
+  newComment.save(function (err, savedComment) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json(savedComment);
+    }
+  });
+});
+
+app.get('/api/comments/:id', function (req, res) {
+// get beer id from url params (`req.params`)
+  var commentId = req.params.id;
+
+
+  // find beer in db by id
+  Comment.findOne({ _id: beerId }, function (err, foundComment) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json(foundComment);
+    }
+  });
+
+
+});
+
+//update Beer
+app.put('/api/comments/:id', function (req, res) {
+    var id = req.params.id;
+    console.log('hit update route');
+    Comment.findById({_id: id}, function (err, foundBeer){
+        if (err) console.log(err);
+        foundComment.author = req.body.author;
+        foundComment.body = req.body.body;
+        foundComment.upvotes = req.body.upvotes;
+        foundComment.save(function (err, saved){
+            if (err) { console.log(err);}
+            res.json(saved);
+        });
+    });
+});
+
+app.delete('/api/comments/:id', function (req, res) {
+  var id = req.params.id;
+  Comment.remove({_id:id}, function (err) {
+    if (err)
+    console.log(err);
+
+  });
+
+});
+
+app.get('*', function (req, res) {
+  res.render('index');
+});
 
 // listen on port 3000
 app.listen(3000, function() {
